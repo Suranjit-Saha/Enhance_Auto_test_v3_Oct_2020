000b7a1e8d34f4fb15126bd4191bfbf2e0d71007
Suranjit Saha <sahasuranjit@gmail.com>
2020-10-27 14:25:07 +1300

    Files added

--- src/test/java/com/utilities/pkg/ExplicitWait.java
+++ src/test/java/com/utilities/pkg/ExplicitWait.java
@@ -1,2 +1,32 @@
-package com.utilities.pkg;public class ExplicitWait {
+package com.utilities.pkg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private WebElement element;
+
+    public ExplicitWait(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement findActiveElement(By locator, int waitTimeInSec) {
+
+        wait = new WebDriverWait(driver,waitTimeInSec);
+
+        try {
+            element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            //System.out.println(locator.toString() + " : Element found");
+        }
+        catch (Exception e) {
+            System.out.println(locator.toString() + " : Element not found");
+        }
+        return element;
+    }
 }
--- src/test/java/com/utilities/pkg/ReadProperties.java
+++ src/test/java/com/utilities/pkg/ReadProperties.java
@@ -1,2 +1,20 @@
-package com.utilities.pkg;public class ReadProperties {
+package com.utilities.pkg;
+
+import org.apache.xmlbeans.SystemProperties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties {
+    private Properties prop = new Properties();
+    private String value = null;
+
+
+    public String readProps (String key) throws IOException {
+        String directory = SystemProperties.getProperty("user.dir") + "//src//test//resources//Properties";
+        FileInputStream fs = new FileInputStream(directory+"//general.properties//");
+        prop.load(fs);
+        value = prop.getProperty(key);
+        return value;
+    }
 }
